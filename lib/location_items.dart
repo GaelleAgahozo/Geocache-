f00b602f8dc4c4d85f8e6862f2318b60c940bae0
Jonathon Qualls <jonathonq02@gmail.com>
2022-09-18 13:27:21 -0500

    Added initial location item
    Needs some things added to it, but I am using the todolistitem as a template

---
+++ lib/location_items.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class Item {
+  const Item({required this.name});
+
+  final String name;
+
+  String abbrev() {
+    return name.substring(0, 1);
+  }
+}
+
+typedef ToDoListChangedCallback = Function(Item item, bool completed);
+typedef ToDoListRemovedCallback = Function(Item item);
+
+class LocationItem extends StatelessWidget {
+  LocationItem(
+      {required this.item,
+      required this.onListChanged,
+      required this.onDeleteItem})
+      : super(key: ObjectKey(item));
+
+  final Item item;
+  final ToDoListChangedCallback onListChanged;
+  final ToDoListRemovedCallback onDeleteItem;
+
+  Color _getColor(BuildContext context) {
+    // The theme depends on the BuildContext because different
+    // parts of the tree can have different themes.
+    // The BuildContext indicates where the build is
+    // taking place and therefore which theme to use.
+
+    return completed //
+        ? Colors.black54
+        : Theme.of(context).primaryColor;
+  }
+
+  TextStyle? _getTextStyle(BuildContext context) {
+    if (!completed) return null;
+
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onListChanged(item, completed);
+      },
+      onLongPress: completed
+          ? () {
+              onDeleteItem(item);
+            }
+          : null,
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(item.abbrev()),
+      ),
+      title: Text(
+        item.name,
+        style: _getTextStyle(context),
+      ),
+    );
+  }
+}
