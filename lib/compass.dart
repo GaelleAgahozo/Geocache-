e1a663b5717c531a270e380ca95ed57b73c7763f
Simon <reidst@hendrix.edu>
2022-09-21 20:44:49 -0500

    Added listener for location; tidied up
    Added a location update listener; refactored method for calculating heading from magnetometer data; added comments for some methods and member variables

--- lib/compass.dart
+++ lib/compass.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:geolocator/geolocator.dart';
@@ -29,25 +31,42 @@ class CompassScreen extends StatefulWidget {
 
 class _CompassScreenState extends State<CompassScreen> {
 
-  _CompassScreenState() {
+  _CompassScreenState() { // Listeners are initialized inside the State's constructor
+    // magnetometer listener (updates angle)
     magnetometerEvents.listen((event) {
-      // transform the magnetometer vector into a compass heading
-      
-      // fix negative zero issues (breaks some calculations)
-      double x = event.x == -0 ? 0 : event.x;
-      double y = event.y == -0 ? 0 : event.y;
-      // find the angle between the x-axis and (x,y)
-      double val = math.atan(y/x);
-      val += x < 0 ? math.pi : 0;
-      val *= 180/math.pi; // radians to degrees
-      val -= 90; // match output to output of Geolocator.bearingBetween(...)
-      _angleOffset = val;
+      _angleOffset = _convertMagnetometerEventToHeading(event);
+      _updateAngle();
     },);
+
+    // geolocator listener (updates position/distance)
+    Geolocator.getPositionStream().listen((Position position) {
+        setState(() {
+          _pos = position;
+        });
+        _updateTargetDist();
+      }
+    );
   }
 
   double _angleOffset = 0.0; // compass heading of device
   double _angle = 0.0; // angle to display on screen
-  Position? _pos;
+  int _targetDist = 0; // distance to target, in meters
+  Position? _pos; // current device position
+
+  double _convertMagnetometerEventToHeading(MagnetometerEvent event) {
+    // transform the magnetometer vector into a compass heading
+      
+    // fix negative zero issues (breaks some calculations)
+    double x = event.x == -0 ? 0 : event.x;
+    double y = event.y == -0 ? 0 : event.y;
+    // find the angle between the x-axis and the vector (x,y)
+    double val = math.atan(y/x);
+    val += x < 0 ? math.pi : 0;
+    val *= 180/math.pi; // radians to degrees
+    // match output to output of Geolocator.bearingBetween(...)
+    val -= 90;
+    return val;
+  }
 
   void _updateAngle() {
     // Outputs of Geolocator.bearingBetween(...):
@@ -65,21 +84,12 @@ class _CompassScreenState extends State<CompassScreen> {
     });
   }
 
-  void _updatePos() async {
-    Position temp = await _determinePosition();
-    setState(() {
-      _pos = temp;
-    });
-    _updateAngle();
-  }
-
-  int _getDistanceToTarget() {
-    _updatePos();
+  void _updateTargetDist() {
     double rawDist = Geolocator.distanceBetween(
       _pos?.latitude ?? 0, _pos?.longitude ?? 0, 
       widget.target.latitude, widget.target.longitude
     );
-    return rawDist.toInt();
+    _targetDist = rawDist.toInt();
   }
   
   @override
@@ -125,7 +135,7 @@ class _CompassScreenState extends State<CompassScreen> {
               ),
             ),
             Text(
-              '${_getDistanceToTarget()}m',
+              '${_targetDist}m',
               style: Theme.of(context).textTheme.headlineSmall,
             ),
           ],
@@ -153,42 +163,4 @@ class _CompassScreenState extends State<CompassScreen> {
     );
   }
 
-}
-
-/// Example position-finding method from the Geolocator API (https://pub.dev/packages/geolocator)
-Future<Position> _determinePosition() async {
-  bool serviceEnabled;
-  LocationPermission permission;
-
-  // Test if location services are enabled.
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    // Location services are not enabled don't continue
-    // accessing the position and request users of the 
-    // App to enable the location services.
-    return Future.error('Location services are disabled.');
-  }
-
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      // Permissions are denied, next time you could try
-      // requesting permissions again (this is also where
-      // Android's shouldShowRequestPermissionRationale 
-      // returned true. According to Android guidelines
-      // your App should show an explanatory UI now.
-      return Future.error('Location permissions are denied');
-    }
-  }
-  
-  if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately. 
-    return Future.error(
-      'Location permissions are permanently denied, we cannot request permissions.');
-  } 
-
-  // When we reach here, permissions are granted and we can
-  // continue accessing the position of the device.
-  return await Geolocator.getCurrentPosition();
 }
