5104602b6b07921fc9e7e81cdd8271fe35b31a08
Jonathon Qualls <jonathonq02@gmail.com>
2022-09-22 23:35:31 -0500

    Combined the two pages and navigation

--- lib/compass.dart
+++ lib/compass.dart
@@ -1,13 +1,47 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 import 'dart:math' as math;
 
 class TestPlaces {
-  static const Position BAILEY_LIBRARY = Position(latitude: 35.10136, longitude: -92.44295, timestamp: null, accuracy: 0, altitude: 0, heading: 0, speed: 0, speedAccuracy: 0);
-  static const Position WEST_GAZEBO = Position(latitude: 35.09928, longitude: -92.44269, timestamp: null, accuracy: 0, altitude: 0, heading: 0, speed: 0, speedAccuracy: 0);
-  static const Position PURPLE_COW = Position(latitude: 35.10252, longitude: -92.43848, timestamp: null, accuracy: 0, altitude: 0, heading: 0, speed: 0, speedAccuracy: 0);
-  static const Position NORTH_POLE = Position(latitude: 90, longitude: 0, timestamp: null, accuracy: 0, altitude: 0, heading: 0, speed: 0, speedAccuracy: 0);
+  static const Position BAILEY_LIBRARY = Position(
+      latitude: 35.10136,
+      longitude: -92.44295,
+      timestamp: null,
+      accuracy: 0,
+      altitude: 0,
+      heading: 0,
+      speed: 0,
+      speedAccuracy: 0);
+  static const Position WEST_GAZEBO = Position(
+      latitude: 35.09928,
+      longitude: -92.44269,
+      timestamp: null,
+      accuracy: 0,
+      altitude: 0,
+      heading: 0,
+      speed: 0,
+      speedAccuracy: 0);
+  static const Position PURPLE_COW = Position(
+      latitude: 35.10252,
+      longitude: -92.43848,
+      timestamp: null,
+      accuracy: 0,
+      altitude: 0,
+      heading: 0,
+      speed: 0,
+      speedAccuracy: 0);
+  static const Position NORTH_POLE = Position(
+      latitude: 90,
+      longitude: 0,
+      timestamp: null,
+      accuracy: 0,
+      altitude: 0,
+      heading: 0,
+      speed: 0,
+      speedAccuracy: 0);
 }
 
 void main() {
@@ -18,18 +52,53 @@ void main() {
 }
 
 class CompassScreen extends StatefulWidget {
-  CompassScreen({super.key});
+  const CompassScreen({super.key});
 
-  Position target = TestPlaces.PURPLE_COW;
-  
+  final Position target = TestPlaces.BAILEY_LIBRARY;
+
   @override
   State<CompassScreen> createState() => _CompassScreenState();
 }
 
 class _CompassScreenState extends State<CompassScreen> {
+  _CompassScreenState() {
+    // Listeners are initialized inside the State's constructor
+    // magnetometer listener (updates angle)
+    magnetometerEvents.listen(
+      (event) {
+        _angleOffset = _convertMagnetometerEventToHeading(event);
+        _updateAngle();
+      },
+    );
 
-  double _angle = 0.0;
-  Position? _pos;
+    // geolocator listener (updates position/distance)
+    Geolocator.getPositionStream().listen((Position position) {
+      setState(() {
+        _pos = position;
+      });
+      _updateTargetDist();
+    });
+  }
+
+  double _angleOffset = 0.0; // compass heading of device
+  double _angle = 0.0; // angle to display on screen
+  int _targetDist = 0; // distance to target, in meters
+  Position? _pos; // current device position
+
+  double _convertMagnetometerEventToHeading(MagnetometerEvent event) {
+    // transform the magnetometer vector into a compass heading
+
+    // fix negative zero issues (breaks some calculations)
+    double x = event.x == -0 ? 0 : event.x;
+    double y = event.y == -0 ? 0 : event.y;
+    // find the angle between the x-axis and the vector (x,y)
+    double val = math.atan(y / x);
+    val += x < 0 ? math.pi : 0;
+    val *= 180 / math.pi; // radians to degrees
+    // match output to output of Geolocator.bearingBetween(...)
+    val -= 90;
+    return val;
+  }
 
   void _updateAngle() {
     // Outputs of Geolocator.bearingBetween(...):
@@ -37,104 +106,63 @@ class _CompassScreenState extends State<CompassScreen> {
     //  90 = east
     // 180 = south
     // -90 = west
-    double bearing = Geolocator.bearingBetween(
-      _pos?.latitude ?? 0, _pos?.longitude ?? 0, 
-      widget.target.latitude, widget.target.longitude
-    );
+    double bearing = Geolocator.bearingBetween(_pos?.latitude ?? 0,
+        _pos?.longitude ?? 0, widget.target.latitude, widget.target.longitude);
 
     setState(() {
-      _angle = bearing;
+      _angle = bearing - _angleOffset;
     });
   }
 
-  void _updatePos() async {
-    Position temp = await _determinePosition();
-    setState(() {
-      _pos = temp;
-    });
-    _updateAngle();
+  void _updateTargetDist() {
+    double rawDist = Geolocator.distanceBetween(_pos?.latitude ?? 0,
+        _pos?.longitude ?? 0, widget.target.latitude, widget.target.longitude);
+    _targetDist = rawDist.toInt();
   }
 
-  int _getDistanceToTarget() {
-    _updatePos();
-    double rawDist = Geolocator.distanceBetween(
-      _pos?.latitude ?? 0, _pos?.longitude ?? 0, 
-      widget.target.latitude, widget.target.longitude
-    );
-    return rawDist.toInt();
-  }
-  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => print('going back'),
-        ),
-        title: const Text('Geocache Locator'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20),
-              child: Text(
-                'Direction to Geocache:',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Text(
+              'Direction to Geocache:',
+              style: Theme.of(context).textTheme.headlineSmall,
             ),
-            Transform.rotate(
-              angle: _angle * (math.pi / 180),
-              child: Icon(
-                Icons.arrow_upward_rounded,
-                size: 200,
-                color: Theme.of(context).primaryColorDark,
-                shadows: [Shadow(
+          ),
+          Transform.rotate(
+            angle: _angle * (math.pi / 180),
+            child: Icon(
+              Icons.arrow_upward_rounded,
+              size: 200,
+              color: Theme.of(context).primaryColorDark,
+              shadows: [
+                Shadow(
                   color: Colors.black38,
                   offset: Offset.fromDirection(1, 8),
                   blurRadius: 2,
-                )],
-              ),
+                )
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Text(
-                'Distance to Target:',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-            ),
-            Text(
-              '${_getDistanceToTarget()}m',
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Text(
+              'Distance to Target:',
               style: Theme.of(context).textTheme.headlineSmall,
             ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1,
-        items: [
-          BottomNavigationBarItem(
-            icon: IconButton(
-              icon: const Icon(Icons.home_rounded), 
-              onPressed: () => print('navigating to homepage'),
-            ),
-            label: 'Home',
           ),
-          BottomNavigationBarItem(
-            icon: IconButton(
-              icon: const Icon(CupertinoIcons.compass_fill), 
-              onPressed: () => print('you are already on this page'),
-            ),
-            label: 'Locator',
+          Text(
+            '${_targetDist}m',
+            style: Theme.of(context).textTheme.headlineSmall,
           ),
         ],
       ),
     );
   }
-
 }
 
 /// Example position-finding method from the Geolocator API (https://pub.dev/packages/geolocator)
@@ -146,7 +174,7 @@ Future<Position> _determinePosition() async {
   serviceEnabled = await Geolocator.isLocationServiceEnabled();
   if (!serviceEnabled) {
     // Location services are not enabled don't continue
-    // accessing the position and request users of the 
+    // accessing the position and request users of the
     // App to enable the location services.
     return Future.error('Location services are disabled.');
   }
@@ -157,18 +185,18 @@ Future<Position> _determinePosition() async {
     if (permission == LocationPermission.denied) {
       // Permissions are denied, next time you could try
       // requesting permissions again (this is also where
-      // Android's shouldShowRequestPermissionRationale 
+      // Android's shouldShowRequestPermissionRationale
       // returned true. According to Android guidelines
       // your App should show an explanatory UI now.
       return Future.error('Location permissions are denied');
     }
   }
-  
+
   if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately. 
+    // Permissions are denied forever, handle appropriately.
     return Future.error(
-      'Location permissions are permanently denied, we cannot request permissions.');
-  } 
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
 
   // When we reach here, permissions are granted and we can
   // continue accessing the position of the device.
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geocaching_app/compass.dart';
 import 'package:geocaching_app/location_page.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Location List'),
+      home: MyHomePage(title: 'Location List'),
     );
   }
 }
@@ -32,13 +33,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    LocationList(),
+    CompassScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(child: LocationList()),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex),),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -51,6 +64,8 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               label: "Compasss"),
         ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
