37cfcbd3e56ad3f0d0f06e73997d8d9b1a3c3214
Gaëlle Agahozo <agahozogaelle4@gmail.com>
2022-12-12 15:37:47 -0600

    Added get location address of the user to help them before they go to their targeted location

---
+++ lib/get_location.dart
@@ -0,0 +1,83 @@
+// import 'package:geolocator/geolocator.dart';
+
+// void getCurrentLocation() async {
+//   var position = await Geolocator.getCurrentPosition(
+//       desiredAccuracy: LocationAccuracy.high);
+//   var lastPosition = await Geolocator.getLastKnownPosition();
+//   print(lastPosition);
+//   setState(() {
+//     locationMessage = "$position.latitude, $position.longitude";
+//   });
+// }
+
+// void setState(Null Function() param0) {}
+
+// final FirebaseAuth auth = FirebaseAuth.instance;
+// var locationMessage = "";
+
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Position _currentPosition;
+  late String _currentAddress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Location"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_currentAddress != null) Text(_currentAddress),
+            ElevatedButton(
+              child: Text("Get location"),
+              onPressed: () {
+                _getCurrentLocation();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _getCurrentLocation() {
+    Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.best,
+            forceAndroidLocationManager: true)
+        .then((Position position) {
+      setState(() {
+        _currentPosition = position;
+        _getAddressFromLatLng();
+      });
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _getAddressFromLatLng() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+
+      Placemark place = placemarks[0];
+
+      setState(() {
+        _currentAddress =
+            "${place.locality}, ${place.postalCode}, ${place.country}";
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+}
